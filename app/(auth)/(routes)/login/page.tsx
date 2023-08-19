a21512bf038fefc596aea67da1bc80571571cd75
manu rodgers <manurodgersgoat@gmail.com>
2023-08-19 12:42:17 +1000

    group routes

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,6 @@
+type LoginPageProps = { name?: string };
+
+const LoginPage = ({ name }: LoginPageProps) => {
+  return <div>LoginPage(unprotected)</div>;
+};
+export default LoginPage;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,6 @@
+type RootPageProps = { name?: string };
+
+const RootPage = ({ name }: RootPageProps) => {
+  return <div>RootPage(protected)</div>;
+};
+export default RootPage;
